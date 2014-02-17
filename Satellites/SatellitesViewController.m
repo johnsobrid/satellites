b7f1c37e18ff6ac021fe96eefe0e676f3d2e2d5d
Bridget Johnson <johnsonbridget.d@gmail.com>
2014-02-18 11:29:48 +1300

    Control Data prints continually to screen (no more need for print data button)

--- Satellites/SatellitesViewController.m
+++ Satellites/SatellitesViewController.m
@@ -9,13 +9,20 @@
 #import "SatellitesViewController.h"
 
 @interface SatellitesViewController ()
+//all the properties for the sliders
 @property (strong, nonatomic) IBOutlet UISlider *panelOneAngleSlider;
 @property (strong, nonatomic) IBOutlet UISlider *panelTwoAngleSlider;
 @property (strong, nonatomic) IBOutlet UISlider *panelOneSpeedSlider;
 @property (strong, nonatomic) IBOutlet UISlider *panelTwoSpeedSlider;
-- (IBAction)HomeButton:(id)sender;
-- (IBAction)printDataButton:(id)sender;
+- (IBAction)panelOneAngleChanged:(id)sender;
+- (IBAction)panelTwoAngleChanged:(id)sender;
+- (IBAction)panelOneSpeedChanged:(id)sender;
+- (IBAction)panelTwoSpeedChanged:(id)sender;
 
+//ids for the buttons
+- (IBAction)HomeButton:(id)sender;
+
+//properties for the labels
 @property (strong, nonatomic) IBOutlet UILabel *panelTwoAngleLabel;
 @property (strong, nonatomic) IBOutlet UILabel *panelOneAngleLabel;
 @property (strong, nonatomic) IBOutlet UILabel *panelOneSpeedLabel;
@@ -29,6 +36,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+   //[self setUpObservers];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,17 +51,31 @@
    [self.panelTwoAngleSlider setValue:0.0];
    [self.panelOneSpeedSlider setValue:0.0];
    [self.panelTwoSpeedSlider setValue:0.0];
+   [self updateOutputData];
    }
 
-- (IBAction)printDataButton:(id)sender {
-   [self setOutputData];
-}
-
--(void)setOutputData
+-(void)updateOutputData
 {
    self.panelOneAngleLabel.text = [NSString stringWithFormat:@"/panelOne/angle %.2f ", [self.panelOneAngleSlider value]];
    self.panelTwoAngleLabel.text = [NSString stringWithFormat:@"/panelTwo/angle %.2f ", [self.panelTwoAngleSlider value]];
    self.panelOneSpeedLabel.text = [NSString stringWithFormat:@"/panelOne/speed %.2f ", [self.panelOneSpeedSlider value]];
+   self.panelTwoSpeedLabel.text = [NSString stringWithFormat:@"/panelTwo/speed %.2f ", [self.panelTwoSpeedSlider value]];
+}
+
+
+- (IBAction)panelOneAngleChanged:(id)sender {
+   self.panelOneAngleLabel.text = [NSString stringWithFormat:@"/panelOne/angle %.2f ", [self.panelOneAngleSlider value]];
+}
+
+- (IBAction)panelTwoAngleChanged:(id)sender {
+   self.panelTwoAngleLabel.text = [NSString stringWithFormat:@"/panelTwo/angle %.2f ", [self.panelTwoAngleSlider value]];
+}
+
+- (IBAction)panelOneSpeedChanged:(id)sender {
+   self.panelOneSpeedLabel.text = [NSString stringWithFormat:@"/panelOne/speed %.2f ", [self.panelOneSpeedSlider value]];
+}
+
+- (IBAction)panelTwoSpeedChanged:(id)sender {
    self.panelTwoSpeedLabel.text = [NSString stringWithFormat:@"/panelTwo/speed %.2f ", [self.panelTwoSpeedSlider value]];
 
 }
